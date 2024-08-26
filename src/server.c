c9260e45d246b00d32d5ce54ea5e415c3671ce7b
skagmltn7 <skagmltn7@hanmail.net>
2024-08-26 17:48:07 +0900

    feat: command put

--- src/server.c
+++ src/server.c
@@ -8,10 +8,12 @@
 #include "../include/message.h"
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
+#define MAX_BUF_SIZE 1024
 
 void connect_client();
-void sync_file_io(char* file_name, const char* content, const char* access_mode);
+void sync_file_io(_Message* message);
+void exec_command(int new_socket);
+void command_get(FILE* fp, int length);
 
 int main(int argc, char const* argv[]){
 	connect_client();
@@ -19,8 +21,7 @@ int main(int argc, char const* argv[]){
 }
 
 void connect_client(){
-	int server_fd, new_socket;
-	ssize_t valread;
+    int server_fd, new_socket;
 	struct sockaddr_in addr;
 	int opt = 1;
 	socklen_t addrlen = sizeof(addr);
@@ -36,7 +37,8 @@ void connect_client(){
 			perror("setsockopt");
 			exit(EXIT_FAILURE);	
 	}
-	
+
+	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(PORT);
@@ -46,7 +48,7 @@ void connect_client(){
 		exit(EXIT_FAILURE);	
 	}
 
-	if(listen(server_fd, 3) <0){
+	if(listen(server_fd, 1) <0){
 		perror("listen");
 		exit(EXIT_FAILURE);	
 	}
@@ -57,25 +59,32 @@ void connect_client(){
 	}
 
     printf("\nConnected to client: %s\n", inet_ntoa(addr.sin_addr));
+    exec_command(new_socket);
+
+    printf("\nCLOSE CONNECTION: %s\n\n", inet_ntoa(addr.sin_addr));
+    close(new_socket);
+    close(server_fd);
+}
+
+void exec_command(int new_socket){
+    ssize_t valread;
+    _Message* message = NULL;
 
 	while(1){
-		_Message* message = (_Message*)malloc(sizeof(_Message));
+		message = (_Message*)malloc(sizeof(_Message));
         if(message == NULL){
             printf("\nMemory allocation failed\n");
             break;
         }
 
-		valread = recv(new_socket, message, sizeof(*message), 0);
-		if(valread==0){
-			close(new_socket);
-			close(server_fd);
-			printf("\nCLOSE CONNECTION: %s\n\n", inet_ntoa(addr.sin_addr));
-			break;
-		}
-
-		//sync_file_io("test.txt", buffer, getMode(message->header.type));
+        valread = recv(new_socket, message, sizeof(*message), 0);
+        if(valread<=0){
+            free(message);
+            return;
+        }
+        sync_file_io(message);
         free(message);
-	}	
+	}
 }
 
 void sync_file_io(char* file_name, const char* content, const char* access_mode){
@@ -84,14 +93,49 @@ void sync_file_io(char* file_name, const char* content, const char* access_mode)
 	if((fp = fopen(file_name, access_mode))==NULL){
 		printf("\n can not open this file \n\n");
 		return;
+void sync_file_io(_Message* message){
+	FILE* fp = NULL;
+
+	if((fp = fopen(message->header.file_name, getMode(message->header.type)))==NULL){
+		perror("\n can not open this file ");
+		return ;
 	}
 
-	printf("The file is opened\n");
-	
-	if(strlen(content)>0){
-		fputs(content, fp);
-		fputs("\n",fp);
+	printf("\nThe file is opened\n\n");
+	fseek(fp, message->body.offset, SEEK_SET);
+
+	if(message->header.type == PUT){
+	    if(strlen(message->body.content)>0){
+		    fputs(message->body.content, fp);
+        }
+	}else if(message->header.type == GET){
+        command_get(fp, message->body.length);
 	}
 
 	fclose(fp);
+}
+
+void command_get(FILE* fp, int length){
+    char* ret = NULL;
+    int length_left = length;
+
+    while(length_left > 0 && !feof(fp)){
+        char buffer[MAX_BUF_SIZE] = {0};
+        int length_read = length > MAX_BUF_SIZE ? MAX_BUF_SIZE : length_left + 1;
+        ret = fgets(buffer, length_read, fp);
+
+        if(ret == NULL){
+            if(feof(fp)) {
+                break;
+            }
+            perror("fgets");
+            return;
+        }
+
+        if(!strcmp(ret,"\n")) continue;
+        printf("%s",ret);
+        length_left -= strlen(ret);
+        if(buffer[strlen(ret)-1]=='\n') length_left++;
+    }
+    printf("\n");
 }
